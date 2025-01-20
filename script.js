07debf1ca94a4edd7f98f250574678771afbfa77
Parbhat Duddy <duddyparbhat@gmail.com>
2025-01-20 16:28:30 +0530

    Added Page

--- script.js
+++ script.js
@@ -46,6 +46,46 @@ var swiper = new Swiper(".mySwiper", {
 
 
 
-// Animation Onload
+// Validate Input
+function applyFilter() {
+    var cat1 = document.getElementById("category1").value;
+    var cat2 = document.getElementById("category2").value;
+    var cat3 = document.getElementById("category3").value;
+    var cat4 = document.getElementById("category4").value;
 
-// Animation Onload
+    var products = document.querySelectorAll(".product");
+
+    products.forEach(function(product) {
+        var match = true;
+
+        if (cat1 && product.getAttribute("data-cat1") !== cat1) {
+            match = false;
+        }
+        if (cat2 && product.getAttribute("data-cat2") !== cat2) {
+            match = false;
+        }
+        if (cat3 && product.getAttribute("data-cat3") !== cat3) {
+            match = false;
+        }
+        if (cat4 && product.getAttribute("data-cat4") !== cat4) {
+            match = false;
+        }
+
+        if (match) {
+            product.style.display = "inline-block";
+        } else {
+            product.style.display = "none";
+        }
+    });
+}
+
+
+// 
+function openCalendar() {
+    flatpickr("#date", {
+      dateFormat: "d-m-Y", // Desired format (dd-mm-yyyy)
+      allowInput: true, // Optional: allows typing in the input
+    }).open(); // Opens the calendar when the input or icon is clicked
+  }
+
+
